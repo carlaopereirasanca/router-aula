065e45d4acdd410319378e9556433d55642c1a60
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2024-08-13 18:45:15 -0300

    apos aula 2 de router

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,9 +1,26 @@
 
+import Post from "componentes/Post";
+import styles from './Inicio.module.css';
+import posts from 'json/posts.json'
+
 export default function Inicio() {
 
-	return (
-        <h1>Olá mundo!</h1>
-	)
+	// Apenas o conteúdo principal da página Inicio,
+	// isto é, os posts !!
 
+	return (
+        	
+		<ul className={styles.posts}>
+
+			{ posts.map( (post)=> {
+				return (
+					<li key={post.id}>
+						<Post post={post} />
+					</li>
+				);
+			} ) }
+
+		</ul>
+	)
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
 
@@ -11,20 +13,27 @@ function AppRoutes() {
 
     <BrowserRouter>
 
-      { /* Colocar o menu fora de todas as rotas,
-           para aparecer em todas as páginas      */}
-
+      { /* Menu fora de todas as rotas, p/ aparecer em todas as páginas */ }
       <Menu />
 
       <Routes>
 
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={ <PaginaPadrao /> }>
+
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+
+        </Route>
+
         <Route path="*" element={<h1>Página não encontrada!</h1>} />
 
       </Routes>
 
+      { /* Rodape fora de todas as rotas, p/ aparecer em todas as páginas */ }
+      <Rodape />
+
     </BrowserRouter>
+    
   )
 }
 
